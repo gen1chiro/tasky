16414af4036a446fbdc42e0e5b39dca51a0fb10d
gen1chiro <juljavellana1218@gmail.com>
2024-11-24 15:39:57 +0800

    Add addOrEditTask & clearForm functions

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const containers = document.querySelectorAll(".column");
-const tests = document.querySelectorAll(".task");
+const tasks = document.querySelectorAll(".task");
+const addTaskBtns = document.querySelectorAll(".add-button");
+const taskDialog = document.querySelector(".task-dialog");
+const confirmBtn = document.querySelector(".confirm");
+const cancelBtn = document.querySelector(".cancel");
+const titleInput = document.getElementById("task-title");
+const descInput = document.getElementById("task-description");
+const priorityInput = document.getElementById("priority");
+
+let activeColumn = null;
+
+const addOrEditTask = () => {
+    const task = document.createElement("div");
+    const taskInfo = `<p class="${priorityInput.value}">${priorityInput.value}</p><h3>${titleInput.value}</h3><p>${descInput.value}</p>`;
+    task.className = "task draggable";
+    task.draggable = true;
+    task.innerHTML = taskInfo;
+    makeTaskDraggable(task);
+    activeColumn.appendChild(task);
+}
+
+const clearForm = () => {
+    titleInput.value = "";
+    descInput.value = "";
+}
+
+const makeTaskDraggable = (task => {
+    task.addEventListener("dragstart", () => {
+        task.classList.add("dragging");
+    });
+
+    task.addEventListener("dragend", () => {
+        task.classList.remove("dragging");
+    });
+});
 
 containers.forEach(container => {
     container.addEventListener("dragover", event => {
         event.preventDefault();
         const element = document.querySelector(".dragging");
         container.appendChild(element);
-    })
+    });
+});
+
+addTaskBtns.forEach(btn => {
+   btn.addEventListener("click", () => {
+       activeColumn = btn.closest('.column');
+       taskDialog.showModal();
+   })
+});
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addOrEditTask();
+    clearForm();
+    activeColumn = null;
+    taskDialog.close();
 })
 
-tests.forEach(test => {
-    test.addEventListener("dragstart", () => {
-        test.classList.add("dragging");
-    });
-
-    test.addEventListener("dragend", () => {
-        test.classList.remove("dragging");
-    });
+cancelBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    clearForm();
+    activeColumn = null;
+   taskDialog.close();
 });
